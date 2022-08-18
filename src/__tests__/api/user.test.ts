2334ca2a4f55ee8c82f1daf60230f2b66bbb2216
Serhii Nahornyi <snah@ciklum.com>
2022-08-18 16:48:37 +0300

    Tests replace mocks

--- src/__tests__/api/user.test.ts
+++ src/__tests__/api/user.test.ts
@@ -1,23 +1,33 @@
-import * as requestApi from "../../api/reqest";
+import { request } from "../../api/reqest";
 import { loginUser } from "../../api/user";
 import { BOOKS_API_BASE_URL } from "../../constants/books-api";
 import { User } from "../../types/user";
 
+jest.mock('../../api/reqest', () => ({
+  request: jest.fn()
+}))
+
 describe('LoginUser', () => {
-  const USER: User = {
+  const mockUser: User = {
     username: 'test',
     avatar: 'test',
     token: 'test',
   };
 
-  it('Should make request to the signin endpoint api and receive a user data', async () => {
-    const requestSpy = jest.spyOn(requestApi, 'request').mockReturnValue(Promise.resolve(USER));
+  beforeEach(() => {
+    (request as jest.Mock).mockResolvedValue(mockUser);
+  });
 
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('Should make request to the signin endpoint api and receive a user data', async () => {
     const username = 'test';
 
     const user = await loginUser(username);
 
-    expect(requestSpy).toBeCalledWith(`${BOOKS_API_BASE_URL}/signin`, 'POST', { username });
-    expect(user).toEqual(USER);
+    expect(request).toHaveBeenCalledWith(`${BOOKS_API_BASE_URL}/signin`, 'POST', { username });
+    expect(user).toEqual(mockUser);
   });
 });
