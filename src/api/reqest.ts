9ea7aa85a11b853eda6dbd4ffdfba23cf5e7a670
Serhii Nahornyi <snah@ciklum.com>
2022-08-18 23:34:05 +0300

    Axios added

--- src/api/reqest.ts
+++ src/api/reqest.ts
@@ -1,9 +1,14 @@
-export const request = async <T>(url: string, method = 'GET', data?: any): Promise<T> => 
-  await fetch(url, {
+import axios from "axios";
+
+export const request = async <T>(url: string, method = 'GET', data?: any): Promise<T> => {
+  const response = await axios({
+    url,
     method,
     headers: {
-      'Content-Type': 'application/json;charset=utf-8',
+      "Content-Type": "application/json;charset=utf-8"
     },
-    body: data ? JSON.stringify(data) : null,
-  })
-  .then<T>(data => data.json());
+    data,
+  });
+
+  return response?.data;
+}
