4ed13abbbf3991d240bc132d449f401e9a98a507
Serhii Nahornyi <snah@ciklum.com>
2022-08-22 13:48:36 +0300

    Request function changed to axios

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,6 @@
+import axios from "axios";
 import { BOOKS_API_BASE_URL } from "../constants/books-api";
 import { User } from "../types/user";
-import { request } from "./reqest";
 
 export const loginUser = async (username: string | null): Promise<User> => 
-  await request<User>(`${BOOKS_API_BASE_URL}/signin`, 'POST', { username });
+  (await axios.post(`${BOOKS_API_BASE_URL}/signin`, { username })).data;
